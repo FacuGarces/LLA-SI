e7a1fd8c60a8955b095a16b5c40404713cbb10f1
Facundo <facundogarces2004@gmail.com>
2025-01-30 16:43:13 -0300

    Fix Overlay

--- js.js
+++ js.js
@@ -1,13 +1,16 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const overlay = document.querySelector("#overlay"); // Seleccionamos la overlay
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
+    overlay.classList.add("visible"); // Mostramos la overlay
 });
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
+    overlay.classList.remove("visible"); // Ocultamos la overlay
 });
 
 // Cerrar el menú al hacer clic fuera de él
@@ -17,5 +20,11 @@ document.addEventListener("click", (event) => {
 
     if (!isClickInsideNav && !isClickOnAbrir) {
         nav.classList.remove("visible");
+        overlay.classList.remove("visible"); // Ocultamos la overlay si se hace clic fuera del menú
     }
+});
+
+// Evitar que el clic dentro del menú cierre el menú
+nav.addEventListener("click", (event) => {
+    event.stopPropagation();
 });
